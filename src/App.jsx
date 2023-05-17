1ec12006915103d28dbf4ba356822c22fd254679
Limon-0007 <limonahmed3023@gmail.com>
2023-05-18 01:53:19 +0600

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <div>
+      <h2 className="text-center text-4xl">This is toy website</h2>
+      <button className="btn btn-outline btn-accent">Button</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
